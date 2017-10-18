2bdcf7af8858d90a079341b3bb3da8b3a3f55748
Joshua Reeder <josh@bleon.com>
2017-10-18 16:20:24 -0500

    j

--- bdtheme/public/js/custom.js
+++ bdtheme/public/js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	// $('header .navbar .container').remove(frappe.render_template("#navbar-breadcrumbs"));
 	$('.main-section').append(frappe.render_template("main-sidebar"));
 	// $('.page-container').remove('.page-head');
-	$('.page-head').prependTo('.layout-main-section')(jQuery);;
+	$('.page-head').prependTo('.layout-main-section')(jQuery);
 
 	$('header').addClass('main-header');
 	$('header .navbar').removeClass('navbar-fixed-top');
