a9a52e2fb4709d46da2a2fe1e28492e55da92d52
Joshua Reeder <josh@bleon.com>
2017-10-19 12:29:45 -0500

    prepend try again

--- bdtheme/public/js/custom.js
+++ bdtheme/public/js/custom.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	// $('header .navbar .container').remove(frappe.render_template("#navbar-breadcrumbs"));
 	$('.main-section').append(frappe.render_template("main-sidebar"));
 	// $('.page-container').remove('.page-head');
-	$('.page-head').prependTo('.layout-main-section');
+	$('.page-head').prependTo(frappe.onload_post_render('.layout-main-section'));
 
 	$('header').addClass('main-header');
 	$('header .navbar').removeClass('navbar-fixed-top');
