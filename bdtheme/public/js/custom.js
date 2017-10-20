4ef1a270fa45c50050ec181495535c39f8e47761
Joshua Reeder <josh@bleon.com>
2017-10-20 15:13:30 -0500

    fix

--- bdtheme/public/js/custom.js
+++ bdtheme/public/js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 	$('.dropdown-help').addClass('hidden');
 	$('#toolbar-user [href*="/index"]').addClass('hidden');
 	$('#toolbar-user [href*="#background_jobs"]').addClass('hidden');
-	$('.dropdown-navbar-new-comments)').addClass('hidden');
+	$('.dropdown-navbar-new-comments').addClass('hidden');
 	$('header .navbar').removeClass('navbar-fixed-top');
 	$('.set-filters .btn').removeClass('text-muted');
 	$('#navbar-breadcrumbs').addClass('hidden');
