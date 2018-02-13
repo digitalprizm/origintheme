583cf0b23821572d8492620986004a1e00380fd7
John Kelley <johnhkelley1@gmail.com>
2018-02-13 12:25:51 -0700

    look for home_page_link

--- bdtheme/api.py
+++ bdtheme/api.py
@@ -4,4 +4,7 @@ import frappe
 import frappe.defaults
 
 def on_session_creation(login_manager):
-	frappe.local.response["home_page"] = "/desk#Form/User/Administrator"
+	info = frappe.db.get_value("User", frappe.local.session_obj.user,
+			["home_page_link"], as_dict=1)
+
+	frappe.local.response["home_page"] = info.home_page_link or "/desk"
